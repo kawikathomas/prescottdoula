f0a3f0dc02b43bd6ebeb3d62893140873727aa9f
David Thomas <dthomas865@gmail.com>
2016-11-16 21:19:19 -0800

    update git hub. i really should be better about doing this

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :messages, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   match '/contacts', to: 'contacts#new', via: 'get'
   resources "contacts", only: [:new, :create]
